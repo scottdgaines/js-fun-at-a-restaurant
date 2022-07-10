3679a4f8e23c23d0735793bf560a9615a475941c
Scott Gaines <scottdgaines@gmail.com>
2022-07-09 18:29:09 -0600

    Pass all tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,38 @@
+function takeOrder(order, deliveryOrders) {
+  deliveryOrders.push(order);
+  if (deliveryOrders.length > 3) {
+    deliveryOrders.splice(3, 1);
+  } else {
+  return deliveryOrders;
+  }
+}
 
+function refundOrder(num, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === num) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  var items = []
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    items.push(deliveryOrders[i].item)
+  }
+return items.join(", ")
+}
+
+function searchOrder(deliveryOrders, item) {
+  var boo = false
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === item) {
+    boo = true }
+  } return boo
+}
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
