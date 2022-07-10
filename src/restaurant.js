7726f7f24128c02088225007f2acc5874243becb
Scott Gaines <scottdgaines@gmail.com>
2022-07-09 21:43:19 -0600

    Pass 11 tests, fail 1, 2 pending

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,36 @@
+function createRestaurant(name) {
+  var menus = {
+    breakfast: [],
+    lunch: [],
+    dinner: []
+  }
+  var pizzaRestaurant = {
+    name: name,
+    menus: menus
+  }
+  return pizzaRestaurant
+}
 
+function addMenuItem(pizzaRestaurant, newItem) {
+    if (newItem.type === "breakfast" && !pizzaRestaurant.menus.breakfast.includes(newItem)) {
+    pizzaRestaurant.menus.breakfast.push(newItem)
+  } else if (newItem.type === "lunch" && !pizzaRestaurant.menus.lunch.includes(newItem)) {
+    pizzaRestaurant.menus.lunch.push(newItem)
+  } else if (newItem.type === "dinner" && !pizzaRestaurant.menus.dinner.includes(newItem)) {
+    pizzaRestaurant.menus.dinner.push(newItem)
+  }
+}
 
+function removeMenuItem(pizzaRestaurant, item, menu) {
+  var result = `No one is eating our ${item} - it has been removed from the ${menu} menu!`
+    for (var i = 0; i < pizzaRestaurant.menus.length; i++) {
+      if (item === pizzaRestaurant.menus.breakfast.name) {
+        pizzaRestaurant.menus.breakfast[0].splice(i, 0)
+      }
+    }
+}
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
